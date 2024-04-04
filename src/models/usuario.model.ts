7d68bee352c376717b20f3c12c89c3d7f687dcd9
Jorge Ivan Garcia Torres <111246512+JorgeGarcia105@users.noreply.github.com>
2024-04-04 11:10:57 -0500

    Creacion de Modelos

---
+++ src/models/usuario.model.ts
@@ -0,0 +1,92 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Usuario extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  primerNombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  segundoNombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  primerApellido: string;
+
+  @property({
+    type: 'string',
+  })
+  segundoApellido?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  celular: string;
+
+  @property({
+    type: 'string',
+  })
+  foto?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  ciudadResidencia: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  direccion: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  clave: string;
+
+  //Datos adicionales para el Administrador
+  @property({
+    type: 'string',
+    required: true,
+  })
+  sede?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  responsabilidades?: string;
+
+  constructor(data?: Partial<Usuario>) {
+    super(data);
+  }
+}
+
+export interface UsuarioRelations {
+  // describe navigational properties here
+}
+
+export type UsuarioWithRelations = Usuario & UsuarioRelations;
