13154d374c7abda899fa65f58b65ffc013797d1b
Eduardo Villamil <eduardo.villamil37830@ucaldas.edu.co>
2024-05-25 11:56:41 -0500

    Eliminación de pqrs

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -3,4 +3,3 @@ export * from './menu.repository';
 export * from './rol-menu.repository';
 export * from './rol.repository';
 export * from './usuario.repository';
-export * from './generar-pqrs.repository';
